0278918ad87651deb3369a22c62594a45a6a7061
dfilipski <danny.filipski@gmail.com>
2021-03-16 11:01:18 -0400

    Add basic Driver functions
    Signed-off-by: dfilipski <danny.filipski@gmail.com>

--- src/csaCalendar/Calendar.java
+++ src/csaCalendar/Calendar.java
@@ -27,4 +27,12 @@ public class Calendar {
     private static boolean isLeapYear(int year) {
         return (year % 4 == 0 ) && ((year % 100 != 0) || (year % 400 == 0));
     }
+
+    public String toString() {
+        String s = "";
+        for (Month m : calendar) {
+            s += m + "\n";
+        }
+        return s;
+    }
 }
--- src/csaCalendar/Driver.java
+++ src/csaCalendar/Driver.java
@@ -1,4 +1,23 @@
 package csaCalendar;
 
+import java.util.Scanner;
+
 public class Driver {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Calendar cal;
+
+        System.out.print("Enter a year greater than 1582: ");
+        int year = sc.nextInt();
+
+        while (year < 1582) {
+            System.out.print("Enter a year greater than 1582: ");
+            year = sc.nextInt();
+        }
+
+        cal = new Calendar(year);
+
+        System.out.println(cal.toString());
+
+    }
 }
