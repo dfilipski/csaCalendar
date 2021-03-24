7ad01eac074fde683d9f4958a5eabd9601515675
Dan Filipski <danny.filipski@gmail.com>
2021-03-23 20:14:52 -0400

    Allow user to make multiple calendars

--- src/csaCalendar/Driver.java
+++ src/csaCalendar/Driver.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 public class Driver {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        while (true) {
+            calendarPrompt(sc);
+
+            System.out.print("Do you want to generate another calendar? ");
+            if (sc.hasNextLine())
+                sc.nextLine();
+            String response = sc.nextLine();
+            if (response.toLowerCase().charAt(0) != 'y')
+                break;
+        }
+    }
+
+    private static void calendarPrompt(Scanner sc) {
         Calendar cal;
 
         System.out.print("Enter a year greater than 1582: ");
@@ -18,6 +31,5 @@ public class Driver {
         cal = new Calendar(year);
 
         System.out.println(cal.toString());
-
     }
 }
